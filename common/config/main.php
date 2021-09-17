646ff684c66d0ec4b3186b6d8989837713b7fa1f
Professional <salohiddin.esanov@mail.ru>
2021-09-17 11:53:57 +0300

    bug

--- common/config/main.php
+++ common/config/main.php
@@ -20,26 +20,18 @@ return [
                 ],
             ],
         ],
-        'mail' => [
+        'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@frontend/mail',
-            'useFileTransport' => false,//set this property to false to send mails to real email addresses
-            //comment the following array to send mail using php's mail function
             'transport' => [
                 'class' => 'Swift_SmtpTransport',
-                'host' => gethostbyname('smtp.gmail.com'),
+                'host' => 'smtp.gmail.com',
                 'username' => 'geostudyuz@gmail.com',
                 'password' => 'omnsnesatlfabwcc',
                 'port' => '587',
                 'encryption' => 'tls',
-                'streamOptions' => [ 
-                    'ssl' => [ 
-                        'allow_self_signed' => true,
-                        'verify_peer' => false,
-                        'verify_peer_name' => false,
-                    ],
-                ],
             ],
+            'viewPath'=>'@common/mail',
+            'useFileTransport' => false,
         ],
     ],
 ];
